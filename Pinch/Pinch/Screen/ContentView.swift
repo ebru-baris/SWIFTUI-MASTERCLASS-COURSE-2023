b9cc7d6be8effd20383c80dfd45b910be929269d
Ebru Baris <114153844+ebru-baris@users.noreply.github.com>
2023-01-10 00:01:36 +0300

    Add files via upload

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -12,8 +12,16 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
     
     // MARK: - FUNCTION
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     
@@ -29,19 +37,32 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
-                // MARK: -1. TAP GESTURE
+                // MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                           resetImageState()
                         }
                     })
+                // MARK: - 2. TAP GESTURE
+                    .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.linear(duration: 1)) {
+                                imageOffset = value.translation
+                            }
+                        }
+                        .onEnded { _ in
+                            if imageScale <= 1 {
+                                resetImageState()
+                                }
+                        }
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
---
+++ Pinch/Pinch/View/InfoPanelView.swift
@@ -0,0 +1,23 @@
+//
+//  InfoPanelView.swift
+//  Pinch
+//
+//  Created by Ebru Barış on 9.01.2023.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoPanelView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPanelView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
