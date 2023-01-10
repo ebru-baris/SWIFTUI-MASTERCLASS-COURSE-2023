88c6bc640ac210e407c3407c7c464021b44d2534
Ebru Baris <114153844+ebru-baris@users.noreply.github.com>
2023-01-10 21:54:48 +0300

    Add files via upload

--- Pinch/Pinch/View/InfoPanelView.swift
+++ Pinch/Pinch/View/InfoPanelView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct InfoPanelView: View {
+    var scale: CGFloat
+    var offset: CGSize
+    
+    @State private var isInfoPanelVisible: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            // MARK: - HOTSPOT
+            Image(systemName: "circle.circle")
+                .symbolRenderingMode(.hierarchical)
+                .resizable()
+                .frame(width: 30, height: 30)
+            
+            Spacer()
+            
+            // MARK: - INFO PANEL
+            HStack(spacing: 2) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.width)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.up.and.down")
+                Text("\(offset.height)")
+                
+                Spacer()
+            }
+            .font(.footnote)
+            .padding(8)
+            .background(.ultraThinMaterial)
+            .cornerRadius(8)
+            .frame(maxWidth: 420)
+            
+            Spacer()
+        }
     }
 }
 
 struct InfoPanelView_Previews: PreviewProvider {
     static var previews: some View {
-        InfoPanelView()
+        InfoPanelView(scale: 1, offset: .zero)
             .preferredColorScheme(.dark)
             .previewLayout(.sizeThatFits)
             .padding()
