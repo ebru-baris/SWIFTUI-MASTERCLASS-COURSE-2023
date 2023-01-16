b14266cc301e0d276f2aff48130878a77c792e4f
Ebru Baris <114153844+ebru-baris@users.noreply.github.com>
2023-01-16 23:39:10 +0300

    Add files via upload

--- Fructus/Fructus/View/StartButtonView.swift
+++ Fructus/Fructus/View/StartButtonView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct StartButtonView: View {
     // MARK: - PROPERTIES
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
     
     // MARK: - BODY
     
     var body: some View {
         Button(action: {
-            print("Exit the onboarding")
+           isOnboarding = false
         }) {
             HStack(spacing: 8) {
                 Text("Start")
